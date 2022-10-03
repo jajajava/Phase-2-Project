50a38cb542eeb454d0cc4d5ef55314d95e82bd89
Scout Cox <coxscout@gmail.com>
2022-10-03 17:36:33 -0400

    Initial styling commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./cc-logo-trans.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="/" method="get">
+          <input
+            className="searchBar"
+            type="text"
+            id="search"
+            placeholder="Find your new favorite meal..."
+          />
+          <button className="searchbtn" type="submit">
+            <img
+              className="searchbtn"
+              src="https://img.icons8.com/ios-glyphs/120/faf8f2/search--v1.png"
+            />
+          </button>
+        </form>
       </header>
+      
+      <div className="search">
+        <div class="dropdown">
+          <button class="dropbtn">filter</button>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>
+        <div class="random">
+          <button class="randombtn">random</button>
+        </div>
+      </div>
     </div>
   );
 }
