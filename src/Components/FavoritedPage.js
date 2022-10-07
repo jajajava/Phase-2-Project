5fac937fb8c9a6b52e2d58e6bfa64e9aa21462d1
Scout Cox <coxscout@gmail.com>
2022-10-07 11:51:01 -0400

    Favorites page styling update, will need to test

--- src/Components/FavoritedPage.js
+++ src/Components/FavoritedPage.js
@@ -1,26 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import RemoveFavorite from "./RemoveFavorite";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
-function FavoritedPage({setRecipeGetter}){
+function FavoritedPage({ setRecipeGetter }) {
+  const [favorites, setFavorites] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/favorited")
+      .then((res) => res.json())
+      .then((data) => setFavorites(data));
+    console.log(favorites);
+  }, []);
 
-    const [favorites, setFavorites] = useState([])
-    useEffect(()=>{fetch('http://localhost:4000/favorited')
-    .then(res => res.json())
-    .then(data => setFavorites(data))
-    console.log(favorites)}, [])
-return(
+  return (
     <div>
-       {favorites.map((each) =>
-         <RemoveFavorite key={each.id} each={each} setRecipeGetter={setRecipeGetter}/>
-         )}
+      <Navbar />
+      <div className="results">
+        <div className="resultsContainer">
+          {favorites.map((each) => (
+            <RemoveFavorite
+              key={each.id}
+              each={each}
+              setRecipeGetter={setRecipeGetter}
+            />
+          ))}
+        </div>
+      </div>
+      <Footer />
     </div>
-)
-
-
+  );
 }
 
-
-
-
-
-export default FavoritedPage
+export default FavoritedPage;
--- src/Components/RemoveFavorite.js
+++ src/Components/RemoveFavorite.js
@@ -1,43 +1,39 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+function RemoveFavorite({ each, setRecipeGetter }) {
+  const [favorite, setFavorite] = useState(true);
 
-function RemoveFavorite({each, setRecipeGetter}){
-
-const [favorite, setFavorite] = useState(true)
-
-
-function handleUnfavorite(){
-  setFavorite(false)
+  function handleUnfavorite() {
+    setFavorite(false);
     fetch(`http://localhost:4000/favorited/${each.id}`, {
-      method: "DELETE"
-    })
-}
+      method: "DELETE",
+    });
+  }
 
+  const navigate = useNavigate();
+  function handleClick() {
+    setRecipeGetter(`${each.id}`);
+    navigate(`${each.name}`);
+  }
 
-const navigate = useNavigate()
-function handleClick(){
-    setRecipeGetter(`${each.id}`)
-    navigate(`${each.name}`)
-   
-   
-}
-
-
-return(
+  return (
     <div>
-      {favorite ? <div>
-      <img onClick={handleClick} src={each.image} alt={`Failed to load ${each.title}`} />
-      <h4>{each.title}   
-      <button onClick={handleUnfavorite}>🗑</button>
-      </h4>
-      </div>
-        : null }
+      {favorite ? (
+        <div>
+          <img
+            onClick={handleClick}
+            src={each.image}
+            alt={`Failed to load ${each.title}`}
+          />
+          <h4>
+            {each.title}
+            <i onClick={handleUnfavorite} class="bx bx-trash"></i>
+          </h4>
+        </div>
+      ) : null}
     </div>
-    
-)
-
+  );
 }
 
-
-export default RemoveFavorite
+export default RemoveFavorite;
