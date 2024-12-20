26fceede2e972a0ac2fbd4a9b844cc8c714ba6eb
David <davidstepanov@yahoo.com>
2024-12-19 22:23:18 -0500

    Moved initial favorite load to App. Not sure if important

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,12 +4,19 @@ import Searched from "./Searched";
 import NotFound from "./NotFound";
 import Recipe from "./Recipe";
 import FavoritedPage from "./FavoritedPage";
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [linkGetter, setLinkGetter] = useState("");
   const [recipeGetter, setRecipeGetter] = useState("");
+
+  //Note: This is to "wake up" the glitch server that's hosting the JSON favorites data
+  useEffect(()=>{
+    fetch('https://caramel-first-verdict.glitch.me/favorited')
+    .then(res => res.json())
+    .then(favs => console.log(favs))
+  }, [])
 
 
   return (
